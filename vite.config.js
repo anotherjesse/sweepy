65b06c77df545f8e16afd0bc24e313dd5e859504
Jesse Andrews <anotherjesse@gmail.com>
2025-08-18 11:46:01 -0700

    listen on 0.0.0.0

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ export default {
   root: "src",
   publicDir: "../public",
   server: {
+    host: '0.0.0.0',  // Required for container access
+    port: 5173,
     allowedHosts: ["jstew.saga-castor.ts.net"],
   },
   build: {
